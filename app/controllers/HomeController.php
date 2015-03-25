9ce090b36a2b2fa893700c48c8c5499b4b489715
Leandro <leandro.b.03@gmail.com>
2015-03-24 22:44:31 -0300

    New teste.

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -26,9 +26,13 @@ class HomeController extends BaseController {
         $data['customers_total'] = $customers->count();
 
         foreach ($customers as $customer) {
-            if ($customer_bracelet = $customer->customer_bracelet()->first()->status != 1) {
-                $data['customers'][$customer->id] = $customer->name;
-            }
+        	if (count($customer->customer_bracelet())) {
+	            $customer_bracelet = $customer->customer_bracelet()->first();
+
+	            if ($customer_bracelet->status != 1) {
+	                $data['customers'][$customer->id] = $customer->name;
+	            }
+	        }
         }
 
         $bracelets = Bracelet::all();
